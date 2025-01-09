c688bf203ffce118aabe756c3220eb0bed66596f
guy kalati <guykalati10@gmail.com>
2025-01-10 01:10:18 +0200

    third

--- User.java
+++ User.java
@@ -44,8 +44,8 @@
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
         name = name.toLowerCase();
-        for(int i = 0 ; i < follows.length ; i++){
-            if (follows[i] != null && follows[i].equals(name)) {
+        for(int i = 0; i < follows.length && follows[i] != null; i++) {
+            if (follows[i].toLowerCase().equals(name)) {
                 return true;
             }
         }
@@ -54,33 +54,43 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        for (int i = 0; i < fCount; i++) {
+        for (int i = 0; i < follows.length; i++) {
             if (follows[i] != null && follows[i].toLowerCase().equals(name.toLowerCase())) {
-                return false; 
+                return false;
+            }
+
+            if (follows[i] == null || follows[i].isEmpty()) {
+                follows[i] = name; 
+                fCount++;          
+                return true;      
             }
         }
-        if (fCount >= maxfCount) {
-            return false; 
-        }
-        follows[fCount] = name;
-        fCount++; 
-        return true;
+    
+        return false;
     }
+    
 
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
     public boolean removeFollowee(String name) {
-        for (int i = 0; i < fCount; i++) { 
+        if(name == null) {
+            return false;
+        }
+        name = name.toLowerCase();
+        for (int i = 0; i < follows.length && follows[i] != null; i++) {
+            follows[i] = follows[i].toLowerCase();
             if (follows[i] != null && follows[i].equals(name)) {
-                for (int j = i; j < fCount - 1; j++) {
+                fCount--; 
+    
+                for (int j = i; j < follows.length - 1; j++) {
                     follows[j] = follows[j + 1];
                 }
-                follows[fCount - 1] = null;
-                fCount--; 
+    
+                follows[follows.length - 1] = null;
                 return true;
             }
         }
-        return false;
+        return false; 
     }
 
 
